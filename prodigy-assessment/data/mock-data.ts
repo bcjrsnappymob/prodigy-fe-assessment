738c0488120b42e32e1ce9b6515642fb9d1ed527
bcjrsnappymob <brian@snappymob.com>
2022-02-28 17:20:36 +0800

    Add mock insurance data

---
+++ prodigy-assessment/data/mock-data.ts
@@ -0,0 +1,26 @@
+import { InsuranceItem } from "../models/insurance";
+
+export const insuranceItems: InsuranceItem[] = [
+    {
+        id: 'starry-night-insurance',
+        stub: 'starry-night-insurance',
+        title: 'Starry Night Insurance',
+        benefits: [ '99% medical converage', 'Premium Insurance Coverage'],
+        description: 'Wish away your pains with this insurance!'
+    },
+    {
+        id: 'active-fast-insurance',
+        stub: 'active-fast-insurance',
+        title: 'Active Fast Insurance',
+        benefits: [ 'Great for quick coverage with minimal payments', 'Cover normal medical issues'],
+        description: 'Wish away your pains with this insurance!'
+    },
+    {
+        id: 'diabetes-go-away-insurance',
+        stub: 'diabetes-go-away-insurance',
+        title: 'Diabetes Go-Away Insurance',
+        benefits: [ 'Premium Insurace for Diabetic Users', 'Cover 99% of Diabetes Medicine and Tests'],
+        description: 'Wish away your pains with this insurance!'
+    },
+];
+
---
+++ prodigy-assessment/models/insurance.ts
@@ -0,0 +1,7 @@
+export type InsuranceItem = {
+    id: string;
+    title: string;
+    stub: string;
+    description: string;
+    benefits: string[];
+}
