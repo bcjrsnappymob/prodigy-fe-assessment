fe5199023d5b7104634ce46b38dbae9b1a41c243
bcjrsnappymob <brian@snappymob.com>
2022-02-28 16:19:52 +0800

    Make base layout

--- prodigy-assessment/components/general/layout.tsx
+++ prodigy-assessment/components/general/layout.tsx
@@ -1,10 +1,17 @@
-import Navbar from "./navbar";
+import Navbar from './navbar';
+import Footer from './footer';
+import { Box } from '@chakra-ui/react';
 
-export default function Layout({ children: any }) {
+export default function Layout({ children }: any) {
     return (
         <div>
             <Navbar></Navbar>
-            <main>{{ children }}</main>
+            <Box
+                minH="100%"
+            >
+                <main>{ children }</main>
+            </Box>
+            <Footer></Footer>
         </div>
     );
 }
--- prodigy-assessment/components/general/navbar.tsx
+++ prodigy-assessment/components/general/navbar.tsx
@@ -1,9 +1,90 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Button, Container, Flex, IconButton, Input, InputGroup, InputRightElement, Stack, useColorModeValue, useDisclosure, Text } from '@chakra-ui/react';
+import {
+    HamburgerIcon,
+    CloseIcon,
+    SearchIcon
+  } from '@chakra-ui/icons';
+  import NextLink from 'next/link';
 
 export default function Navbar() {
+    const { isOpen, onToggle } = useDisclosure()
     return (
-        <Box>
-            
-        </Box>
-    );
+      <Box>
+        <Container maxW={'7xl'}>
+          <Flex
+            bg={useColorModeValue('white', 'gray.800')}
+            color={useColorModeValue('gray.600', 'white')}
+            minH={'60px'}
+            py={{ base: 2 }}
+            px={{ base: 4 }}
+            align={'center'}>
+            <Flex
+              flex={{ base: 1, md: 'auto' }}
+              ml={{ base: -2 }}
+              display={{ base: 'flex', md: 'none' }}>
+              <IconButton
+                onClick={onToggle}
+                icon={
+                  isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
+                }
+                variant={'ghost'}
+                aria-label={'Toggle Navigation'}
+              />
+            </Flex>
+            <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
+              <NextLink href={'/'} passHref>
+                <Text
+                  cursor={'pointer'}
+                  fontFamily={'heading'}
+                  fontWeight={'bold'}
+                  color={useColorModeValue('gray.800', 'white')}>
+                  PRODIGINOW
+                </Text>
+              </NextLink>
+            </Flex>
+  
+            <Stack
+              flex={{ base: 1, md: 0 }}
+              justify={'flex-end'}
+              direction={'row'}
+              spacing={6}>
+              <InputGroup w={60}>
+                <Input
+                  id="search"
+                  type="search"
+                  placeholder={'Search'}
+                  borderRadius={'full'}
+                  fontSize={14}
+                />
+                <InputRightElement as="button" type="submit" cursor="pointer">
+                  <NextLink href={'/products/search'} passHref>
+                    <SearchIcon color="black" />
+                  </NextLink>
+                </InputRightElement>
+              </InputGroup>
+              <Button
+                fontSize={'sm'}
+                fontWeight={400}
+                variant={''}
+                _hover={{textDecoration: 'none'}}>
+                Sign In
+              </Button>
+              <Button
+                display={{ base: 'none', md: 'inline-flex' }}
+                fontSize={'sm'}
+                fontWeight={600}
+                color={'white'}
+                bg={'proPr.30'}
+                borderRadius={'full'}
+                px={6}
+                _hover={{
+                  bg: 'proPr.20',
+                }}>
+                Register
+              </Button>
+            </Stack>
+          </Flex>
+        </Container>
+      </Box>
+    )
 }
